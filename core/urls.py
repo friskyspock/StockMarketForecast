f043db33b1c7f479ebf7d3d02978e6e2512e3b17
friskyspock <aniketms42@outlook.com>
2023-07-02 14:13:35 +0530

    updated

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('retrain/', views.retrain, name='retrain'),
     path('data/',views.send_data,name='send_data'),
     path('info/',views.send_info,name='send_info'),
-    path('about/',views.about,name='about'),
+    path('about/',views.about,name='about')
 ]
